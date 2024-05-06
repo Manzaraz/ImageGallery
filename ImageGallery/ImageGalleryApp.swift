e0b92a9f467e5c442586a172b38f064cd8547bbb
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-06 17:56:14 -0300

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Christian Manzaraz on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
